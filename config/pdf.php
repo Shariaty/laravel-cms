6103618a77ea529e94465c809a07aecc2fad15b8
Shariaty <ala.sharyaty@gmail.com>
2021-03-06 14:58:33 +0330

    Create pdf config file

---
+++ config/pdf.php
@@ -0,0 +1,10 @@
+return [
+	'mode'                  => 'utf-8',
+	'format'                => 'A4',
+	'author'                => '',
+	'subject'               => '',
+	'keywords'              => '',
+	'creator'               => 'cms',
+	'display_mode'          => 'fullpage',
+	'tempDir'               => base_path('storage/app/mpdf')
+];
